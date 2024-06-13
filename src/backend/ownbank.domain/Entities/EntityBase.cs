89225fb0fef43c62c8961ce820200bf7c7fce983
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2024-06-13 18:45:35 -0300

    "feat(EntityUser): adiciona entidade usuario"

---
+++ src/backend/ownbank.domain/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ownbank.domain.Entities
+{
+    public class EntityBase
+    {
+        public long Id { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
