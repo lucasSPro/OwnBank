c365aab0a870215be4fca7d40399bbd99e445b31
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2024-09-21 12:02:49 -0300

    add database migration

---
+++ src/backend/GestorAvaliacao.infrastructure/Migrations/Versions/DatabaseVersions.cs
@@ -0,0 +1,8 @@
+﻿
+namespace GestorAvaliacao.Infrastructure.Migrations.Versions
+{
+    public abstract class DatabaseVersions
+    {
+        public const int TABLE_USER = 1;
+    }
+}
---
+++ src/backend/GestorAvaliacao.infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,12 @@
+﻿using FluentMigrator;
+namespace GestorAvaliacao.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_USER, "Create table User")]
+    internal class Version0000001 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
