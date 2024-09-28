565d229f46c80b839780a132b4019a05d08e7fcf
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2024-09-28 20:23:54 -0300

    Adiciona primeiro teste para a validacao de registro de usuarios

---
+++ src/tests/Validador/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using GestorAvaliacao.Application.UseCases.Register;
+using GestorAvaliacao.Exceptions;
+using UtilidadesComunsTestes.Requests;
+
+
+namespace Validador.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        
+    }
+}
